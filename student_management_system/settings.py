c6894b28a7e807cc6614f611290a742009d7fe54
Zanib <zanibmalhi@gmail.com>
2025-07-24 15:39:37 +0500

    Initial commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -66,10 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'student_management_app.context_processors.user_role_processor', # Add this
-
             ],
-
         },
     },
 ]
@@ -136,4 +133,3 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
